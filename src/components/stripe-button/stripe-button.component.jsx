b9c803ecf1d62db9ba0610e4b34200d3da944143
Arpit Bhadoriya <arpit.bhadoriya@ibm.com>
2021-05-30 12:17:23 +0530

    Added stripe payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51Iwh04SHotkBLx2Gk7d1iHlpsxCK1NnWRp9k1ZOQ2bKMxTk7nNcTksQyFQp386UhNI2JL00tjUgmySd7Qf1rNN0F00eqjfbnmn';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
