a46a4554e786c39cb3f966bb0716ce05942a798e
Arpit <arpitbhadoriya@hotmail.com>
2021-05-30 20:26:01 +0530

    Added styled components in app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.util';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
@@ -19,6 +22,7 @@ class App extends React.Component {
 
 	componentDidMount() {
 		const { setCurrentUser } = this.props;
+
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
@@ -54,8 +58,8 @@ class App extends React.Component {
 							this.props.currentUser ? (
 								<Redirect to='/' />
 							) : (
-									<SignInAndSignUpPage />
-								)
+								<SignInAndSignUpPage />
+							)
 						}
 					/>
 				</Switch>
@@ -72,4 +76,7 @@ const mapDispatchToProps = dispatch => ({
 	setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App);
